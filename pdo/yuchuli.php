7ae18e0bc6b6e8b62d091d430a093cdd5aa4c035
wkk <422598178@qq.com>
2017-07-22 12:04:46 +0800

    daydayup

--- pdo/yuchuli.php
+++ pdo/yuchuli.php
@@ -23,18 +23,21 @@
 		//一、创建预处理对象
 		$statement = $pdo->prepare('insert into myboke(title,content,images) values(:title,:content,:images)');
 
-		//二、绑定参数
-		$statement->bindParam(':title',$t);
-		$statement->bindParam(':content',$c);
-		$statement->bindParam(':images',$i);
+		//二、绑定参数  bindParam(引用传参) 或者bindValue(引用传值)   
+		// $statement->bindParam(':title',$t);
+		// $statement->bindParam(':content',$c);
+		// $statement->bindParam(':images',$i);
 
 		//三、准备参数
-		$t = 'state title';
-		$c = 'state content';
-		$i = 'state.png';
+		// $t = $_GET['title'];
+		// $c = $_GET['content'];
+		// $i = $_GET['images'];
 
-		//四、执行预处理
-		$statement->execute();
+		//四、执行预处理   可以直接传关联数组执行(便于表单提交的数据)
+		// $statement->execute();
+		$statement->execute($_POST);
+
+		echo $statement->rowCount();
 
 	}catch(PDOException $e){
 		echo $e->getMessage();
